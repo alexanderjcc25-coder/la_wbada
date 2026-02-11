dc0a19478121ecbb6c706a2ebb9eea4431c402b0
alexander castro <alexanderjcc25@gmail.com>
2026-02-11 10:24:37 +0100

    primo comit

---
+++ calculadora/cabecita.js
@@ -0,0 +1,17 @@
+const botones = document.querySelectorAll('.btn');
+
+botones.forEach(boton => {
+    boton.addEventListener('click', () => {
+        const display = document.querySelector('.display');
+        const valorBoton = boton.textContent;
+
+        let valorDisplay = display.textContent;
+
+        display.textContent = valorDisplay + valorBoton;
+        //alert(valorBoton)
+    })
+
+})
+
+
+
---
+++ scrip.js
@@ -0,0 +1,13 @@
+
+
+document.querySelector('#label-spoiler').addEventListener('click', function() {
+    const spoiler = document.getElementById('spoiler')
+    spoiler.removeAttribute("hidden");
+})
+function putas(color) {
+      document.body.style.backgroundColor = color;
+    }
+function mycolor(color) {
+      document.body.button.style.backgroundColor = color;
+    }
+    
